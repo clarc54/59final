c5aca3ced52a838db88f19d46e628f56576a2008
clarc54 <chris20topher@yahoo.com>
2015-12-16 13:45:51 -0500

    Update final-CTC.java

--- final-CTC.java
+++ final-CTC.java
@@ -1,6 +1,6 @@
 //Christopher Clark
 //CST112 Final
-
+Lobster alpha, bravo, charlie, delta
 float surface;
 
 //Setup
@@ -27,16 +27,39 @@ void scene(){
 
 //Reset scene
 void reset() {
-  background(0,0,250);
-  fill(0,255,0);
-  rect(0,surface, width, height-surface);
-  fill(204,204,0);
-  ellipse(60,60,60,60);
-  fill(0);
-  text("Christopher Clark", height, width/2+150);
+//reset surface
+surface= random(height/4, height/2);
+//Lobsters
+alpha= new Lobster("Alpha", width/2);
+bravo= new Lobster("Bravo", width/4);
+charlie= new Lobster("Charlie", width*1/4);
+delta= new Lobster( "Delta", width *3/4);
 }
+
 
 //Reset scene key
 void keyPressed() {
   if (key == 'r') reset();
 }
+
+//Lobster class
+class Lobster {
+  // Coordinates
+  float x,y;
+  // Speed
+  float dx=0,dy=0;
+  float w=30,h=30;
+  //legs
+  int legs=10;
+  String name="";
+  // color
+  float r,g,b;
+  
+  Lobster( String l, float x ) {
+    this.name=  l;
+    this.x=x;
+    bottom();
+    r=  random(100, 255);
+    g=  random(0, 100);
+    b=  random(100, 250);
+  }
