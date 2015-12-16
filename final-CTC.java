2d30551ac489ac0423b6372afb0e609a5c56f5fa
clarc54 <chris20topher@yahoo.com>
2015-12-16 13:31:06 -0500

    Update final-CTC.java

--- final-CTC.java
+++ final-CTC.java
@@ -1,2 +1,42 @@
 //Christopher Clark
 //CST112 Final
+
+float surface;
+
+//Setup
+void setup(){
+  size(640,480);
+  surface= random(height/4, height/2);
+  reset();
+}
+
+//Scene
+void draw(){
+  scene();
+  
+}
+void scene(){
+  background(0,0,250);
+  fill(0,255,0);
+  rect(0,surface, width, height-surface);
+  fill(204,204,0);
+  ellipse(60,60,60,60);
+  fill(0);
+  text("Christopher Clark", height, width/2+150);
+}
+
+//Reset scene
+void reset() {
+  background(0,0,250);
+  fill(0,255,0);
+  rect(0,surface, width, height-surface);
+  fill(204,204,0);
+  ellipse(60,60,60,60);
+  fill(0);
+  text("Christopher Clark", height, width/2+150);
+}
+
+//Reset scene key
+void keyPressed() {
+  if (key == 'r') reset();
+}
